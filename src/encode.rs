714ac197709204a0a39d61604793d5df2648b1c3
JJsnek <janalelavojvodic@gmail.com>
2026-04-11 01:38:13 +0200

    encode done

---
+++ src/encode.rs
@@ -0,0 +1,24 @@
+use ark_bls12_381::Fr;
+use ark_ff::Zero;
+
+pub fn encode(
+    message: &Vec<Fr>,
+    graph: &Vec<Vec<usize>>,
+)->Vec<Fr>{
+
+    let m=graph.len();
+    //initialize codeword with zeros
+    let mut codeword=vec![Fr::zero();m];//m komada
+    
+    //for each right node
+    for j in 0..m{
+        let neighbors=&graph[j];
+
+        let mut sum=Fr::zero();
+        for &i in neighbors{
+            sum+=message[i];
+        }
+        codeword[j]=sum;
+    }
+    codeword
+}
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ mod encode;
 use ark_bls12_381::Fr;
 use ark_ff::{UniformRand};
 use rand::thread_rng;
+
 use graph::generate_random_expander;
+use encode::encode;
 
 fn main() {
     println!("Hello, world!");
+
     let mut rng=thread_rng();
     let a=Fr::rand(&mut rng);
     let b=Fr::rand(&mut rng);
@@ -16,7 +19,15 @@ fn main() {
 
     println!("a + b = {:?}",c);
 
-    let graph=generate_random_expander(10,20,3);
+    let n=5; let m=20; let d=3;
+
+    let message:Vec<Fr>=(0..n).map(|_|Fr::rand(&mut rng)).collect();
+    println!("Message {:?}", message);
+
+    let graph=generate_random_expander(n,m,d);
     println!("{:?}", graph);
+
+    let codeword=encode(&message, &graph);
+    println!("Codeword: {:?}", codeword);
 
 }
